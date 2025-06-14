f9ee78af304a2ed7b89aa42887841f8c2b35d60c
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-14 19:00:16 +0300

    03. usefull-things-090-flip-gallery. Bg, fix.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
-
 const scroller = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
-    smooth: true
+    smooth: true,
 });
-
 
 gsap.registerPlugin(Flip);
 
 CustomEase.create("cubic", "0.83, 0, 0.17, 1");
 
 const gallery = document.querySelector(".img-gallery-container");
+const wrapper = document.querySelector(".img-gallery");
 const images = gsap.utils.toArray(".img");
 
 document.querySelector(".btn").addEventListener("click", () => {
@@ -19,31 +18,30 @@ document.querySelector(".btn").addEventListener("click", () => {
 
     images.forEach((img, i) => {
         img.classList.toggle("reorder");
+
+        if (img.classList.contains("reorder")) {
+            gsap.set(img, {
+                rotate: i * 3,
+                duration: 0.5,
+                ease: "power2.out",
+            });
+        } else {
+            gsap.to(img, {
+                rotate: 0,
+                duration: 1,
+                delay: -1,
+                ease: "power2.out",
+            });
+        }
     });
 
     Flip.from(state, {
         absolute: true,
         duration: 2,
         stagger: 0.05,
-        ease: "cubic"
+        ease: "cubic",
+        onComplete: () => {
+            scroller.scrollTo(0, { duration: 0 });
+        },
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
